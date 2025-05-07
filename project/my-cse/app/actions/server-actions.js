06ae5ac9d1fd1efa699d0bd9caa0d821bf9a9b74
Rey-Key <sa2208379@qu.edu.qa>
2025-05-07 17:43:43 +0300

    upp

--- project/my-cse/app/actions/server-actions.js
+++ project/my-cse/app/actions/server-actions.js
@@ -9,4 +9,42 @@ export async function getAllCoursesAction() {
 export async function getCourseByIdAction(courseId) {
     return await mycseRepo.getCourseById(courseId)
 }
+export async function getCoursesByCategoryAction(category) {
+    return await mycseRepo.getCoursesByCategory(category)
+}
+export async function getOfferedCoursesAction(semesterOfferingId) {
+    return await mycseRepo.getOfferedCourses(semesterOfferingId)
+}
+export async function getCourseSectionsAction(courseCode) {
+    return await mycseRepo.getCourseSections(courseCode)
+}
+
+// get completed courses for a given student
+export async function getCompletedCoursesAction(studentId) {
+    return await mycseRepo.getCompletedCourses(studentId)
+}
+// get all courses for a given student
+export async function getAllCoursesForStudentAction(studentId) {
+    return await mycseRepo.getAllCoursesForStudent(studentId)
+}
+//get in progress courses for a given student
+export async function getInProgressCoursesAction(studentId) {
+    return await mycseRepo.getInProgressCourses(studentId)
+}
+// get pending courses for a given student
+export async function getPendingCoursesAction(studentId) {
+    return await mycseRepo.getPendingCourses(studentId)
+}
+//student’s total completed credits
+export async function getCompletedCreditsAction(studentId) {
+    return await mycseRepo.getCompletedCredits(studentId)
+}
+// get course prerequisites for a given course
+export async function getCoursePrerequisitesAction(courseCode) {
+    return await mycseRepo.getCoursePrerequisites(courseCode)
+}// get all sections for a given course
+export async function getAllSectionsAction(courseCode) {
+    return await mycseRepo.getAllSections(courseCode)
+} 
+
 
